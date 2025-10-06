a1d19ff2b976dcdb2b17438c9d8adc39a93f542d
truant4 <askantoine7@gmail.com>
2025-10-06 08:17:59 +0100

    changed the python file to demonstrate euclids algorithm

--- simplepy.py
+++ simplepy.py
@@ -1,1 +1,14 @@
-print("hello world, I am a python program!")
+n = int(input("Entrer une nombre: "))
+print(n)
+premier = True
+
+
+for diviseur in range(2,n):
+    reste = n % diviseur
+    if reste == 0:
+        print("n, n'est pas premier car divisible par", diviseur)
+        premier = False
+
+if premier:
+    print(n, "est premier")
+         
